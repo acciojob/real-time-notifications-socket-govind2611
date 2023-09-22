bbe30ed813c81bee8092d9a55b94f8d4d1b63cc8
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-22 07:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// complete the code
 const statusDiv = document.getElementById("status");
 const messageInput = document.getElementById("message-input");
 const sendButton = document.getElementById("send-button");
@@ -7,12 +6,52 @@ const notificationsDiv = document.getElementById("notifications");
 const wsUrl = "wss://socketsbay.com/wss/v2/1/demo/";
 let socket;
 
-const displayNotification = (message) => {};
+const displayNotification = (message) => {
+  const notification = document.createElement("div");
+  notification.textContent = message;
+  notificationsDiv.appendChild(notification);
+};
 
-const connectWebSocket = () => {};
+const connectWebSocket = () => {
+  socket = new WebSocket(wsUrl);
 
-sendButton.addEventListener("click", () => {});
+  socket.onopen = () => {
+    statusDiv.textContent = "Connected";
+    messageInput.disabled = false;
+    sendButton.disabled = false;
+  };
 
-disconnectButton.addEventListener("click", () => {});
+  socket.onmessage = (event) => {
+    const message = event.data;
+    displayNotification(message);
+  };
+
+  socket.onclose = () => {
+    statusDiv.textContent = "Disconnected";
+    messageInput.disabled = true;
+    sendButton.disabled = true;
+
+    // Automatically attempt to reconnect after 10 seconds
+    setTimeout(() => {
+      statusDiv.textContent = "Reconnecting...";
+      connectWebSocket();
+    }, 10000);
+  };
+};
+
+sendButton.addEventListener("click", () => {
+  const message = messageInput.value.trim();
+  if (message !== "") {
+    socket.send(message);
+    messageInput.value = "";
+  }
+});
+
+disconnectButton.addEventListener("click", () => {
+  if (socket) {
+    socket.close();
+    statusDiv.textContent = "Disconnecting...";
+  }
+});
 
 connectWebSocket();
